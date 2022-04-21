6e3a225bd9ec26e2ae1e83f6cc0cd368bcc3e48d
shibamoto <silverbirder@gmail.com>
2022-04-21 23:11:07 +0900

    :construction: Try integration firestore

--- src/teams/content/src/App.tsx
+++ src/teams/content/src/App.tsx
@@ -1,5 +1,30 @@
+//@ts-ignore
+import { FirestoreCollection } from "react-firestore";
+
 const App = () => {
-  return <>Hello World</>;
+  return (
+    <>
+      <FirestoreCollection
+        path="tweets"
+        limit={100}
+        //@ts-ignore
+        render={({ isLoading, data }) => {
+          return isLoading ? (
+            <>loading</>
+          ) : (
+            <div>
+              <h1>Tweet</h1>
+              <div>
+                {data.map((tweet: any) => (
+                  <div key={tweet.id}>{tweet.text}</div>
+                ))}
+              </div>
+            </div>
+          );
+        }}
+      />
+    </>
+  );
 };
 
 export default App;
--- src/teams/content/src/index.tsx
+++ src/teams/content/src/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from '@firebase/app';
+import '@firebase/firestore';
+//@ts-ignore
+import { FirestoreProvider } from 'react-firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <FirestoreProvider firebase={firebase}>
     <App />
-  </React.StrictMode>
+  </FirestoreProvider>
 );
 
 reportWebVitals();
