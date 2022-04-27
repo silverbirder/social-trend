9b70604db1cf1f5625c35b334d7ba9b8f3ccf010
shibamoto <silverbirder@gmail.com>
2022-04-27 22:41:57 +0900

    :recycle: refactor

---
+++ src/frontend/packages/team-content/src/index.ts
@@ -0,0 +1,5 @@
+import { XContent } from "./components/XContent/XContent";
+
+export { XContent } from "./components/XContent/XContent";
+
+customElements.define("x-content", XContent);
---
+++ src/frontend/packages/team-search/src/index.ts
@@ -0,0 +1,5 @@
+import { XSearch } from "./components/XSearch/XSearch";
+
+export { XSearch } from "./components/XSearch/XSearch";
+
+customElements.define("x-search", XSearch);
