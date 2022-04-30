bea717738ffeea973873914f1baa9dd2c0d3e5f5
shibamoto <silverbirder@gmail.com>
2022-04-30 18:42:03 +0900

    :construction: Coding search form

--- src/frontend/packages/team-search/src/components/XSearch/App.tsx
+++ src/frontend/packages/team-search/src/components/XSearch/App.tsx
@@ -1,14 +1,63 @@
-const App = () => {
-  fetch("http://localhost:4002").then((a) => {
-    a.json().then((j) => {
-      console.log(j);
-    });
-  });
+import React from "react";
+import { useState } from "react";
+
+type Condition = {
+  keyword: string;
+};
+const newCondition = () => {
+  return { keyword: "" };
+};
+
+type KeywordSearchProps = {
+  condition: Condition;
+  setCondition: (condition: Condition) => void;
+};
+
+const KeywordSearch = React.memo((props: KeywordSearchProps) => {
+  console.log("render KeywordSearch");
+  const { condition, setCondition } = props;
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newCond = { ...condition };
+    newCond.keyword = e.target.value;
+    setCondition(newCond);
+  };
+  return (
+    <input
+      type="text"
+      placeholder="Enter keyword"
+      value={condition.keyword}
+      onChange={onChange}
+    ></input>
+  );
+});
+
+type SearchButtonProps = {
+  condition: Condition;
+};
+
+const SearchButton = React.memo((props: SearchButtonProps) => {
+  console.log("render SearchButton");
+  const { condition } = props;
+  const onClick = (_: React.MouseEvent) => {
+    console.log(condition);
+  };
+  return <input type="button" value="Search" onClick={onClick}></input>;
+});
+
+const SearchForm = React.memo(() => {
+  console.log("render SearchForm");
+  const [condition, setCondition] = useState<Condition>(newCondition());
   return (
     <>
-      <div>hello</div>
+      <KeywordSearch condition={condition} setCondition={setCondition} />
+      <SearchButton condition={condition} />
     </>
   );
-};
+});
+
+const App = React.memo(() => {
+  console.log("render App");
+  return <SearchForm />;
+});
 
 export default App;
