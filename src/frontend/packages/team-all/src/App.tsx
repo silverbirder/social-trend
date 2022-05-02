58a0629dbfe6a3007e624e85f82b588913a01c11
shibamoto <silverbirder@gmail.com>
2022-05-02 16:21:42 +0900

    :construction: Coding content element

--- src/frontend/packages/team-all/src/App.tsx
+++ src/frontend/packages/team-all/src/App.tsx
@@ -1,22 +1,41 @@
-// import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import("content/App").then((module) => {
-  const { XContent } = module;
-  customElements.define("x-content", XContent);
-});
-import("search/App").then((module) => {
-  const { XSearch } = module;
-  customElements.define("x-search", XSearch);
-  document.querySelector('x-search')?.addEventListener('search', (e) => {
-    console.log(e);
-  });
-});
+type SearchResultProps = {
+  data: any[];
+};
+const SearchResult = (props: SearchResultProps) => {
+  const { data } = props;
+  return (
+    <>
+      {data.map((d) => (
+        <div key={d.id}>
+          {d.id}: {d.text}
+        </div>
+      ))}
+    </>
+  );
+};
 
 const App = () => {
-  // const [contents, setContents] = useState(null);
+  const [searchResult, setSearchResult] = useState<any[]>([]);
+  useEffect(() => {
+    import("content/App").then((module) => {
+      const { XContent } = module;
+      customElements.define("x-content", XContent);
+    });
+    import("search/App").then((module) => {
+      const { XSearch } = module;
+      customElements.define("x-search", XSearch);
+      const xSearchElement = document.querySelector("x-search");
+      xSearchElement?.addEventListener("search", ((e: CustomEvent) => {
+        setSearchResult(e.detail);
+      }) as EventListener);
+    });
+  }, []);
   return (
     <>
       <x-search></x-search>
+      <SearchResult data={searchResult} />
     </>
   );
 };
