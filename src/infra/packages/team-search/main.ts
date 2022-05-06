f8a187a9f228b1ec98b097e1f4df67152c47fb1f
shibamoto <silverbirder@gmail.com>
2022-05-06 23:12:21 +0900

    :bricks: Add infra code

---
+++ src/infra/packages/team-search/main.ts
@@ -0,0 +1,77 @@
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+import {
+  CloudRunService,
+  GoogleProvider,
+  DataGoogleIamPolicy,
+  CloudRunServiceIamPolicy,
+} from "@cdktf/provider-google";
+
+import * as path from "path";
+import * as fs from "fs";
+import "dotenv/config";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    // define resources here
+    const imagename = "gcr.io/cloudrun/hello";
+
+    const credentialsPath = path.join(process.cwd(), "google.json");
+    const credentials = fs.existsSync(credentialsPath)
+      ? fs.readFileSync(credentialsPath).toString()
+      : "{}";
+
+    const local = "asia-east1"; //region
+    const projectId = process.env.GOOGLE_PROJECT_ID;
+    new GoogleProvider(this, "GoogleAuth", {
+      region: local,
+      zone: local + "-c",
+      project: projectId,
+      credentials,
+    });
+
+    const cloudrunsvcapp = new CloudRunService(this, "GcpCDKCloudrunsvc", {
+      location: local,
+      name: "gcpcdktfcloudrunsvc2020",
+      template: {
+        spec: {
+          containers: [
+            {
+              image: imagename,
+            },
+          ],
+        },
+      },
+    });
+
+    const policy_data = new DataGoogleIamPolicy(this, "datanoauth", {
+      binding: [
+        {
+          role: "roles/run.invoker",
+          members: ["allUsers"],
+        },
+      ],
+    });
+
+    new CloudRunServiceIamPolicy(this, "runsvciampolicy", {
+      location: local,
+      project: cloudrunsvcapp.project,
+      service: cloudrunsvcapp.name,
+      policyData: policy_data.policyData,
+    });
+
+    // new TerraformOutput(this, "cdktfcloudrunUrl", {
+    //   value: "" + cloudrunsvcapp.fqn + ".status[0].url",
+    // });
+
+    // new TerraformOutput(this, "cdktfcloudrunUrlN", {
+    //   value: cloudrunsvcapp.status.get(0).url,
+    // });
+  }
+}
+
+const app = new App();
+new MyStack(app, "gcp-cloud-run");
+app.synth();
